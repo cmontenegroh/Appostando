ac7494530fab6a34b6e6d8e5309d730179f0567f
Carlos Montenegro <cmontenegroh@MacBook-Pro-de-Carlos.local>
2025-11-19 16:00:04 +0100

    Comienzo de empaquetado para web

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
 
@@ -57,6 +58,66 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
+
+// import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+// import { useRouter } from 'expo-router';
+
+// export default function HomeScreen() {
+//   const router = useRouter();
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>🏆 Bienvenido a APPostando</Text>
+//       <Text style={styles.subtitle}>
+//         Tu asistente de cálculo para apuestas inteligentes
+//       </Text>
+
+//       <TouchableOpacity 
+//         style={styles.button} 
+//         onPress={() => router.push('/calculator')}
+//       >
+//         <Text style={styles.buttonText}>Ir a Calculadora</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     padding: 20,
+//     backgroundColor: '#f2f2f2',
+//   },
+//   title: {
+//     fontSize: 28,
+//     fontWeight: 'bold',
+//     color: '#1e90ff',
+//     marginBottom: 10,
+//   },
+//   subtitle: {
+//     fontSize: 16,
+//     color: '#555',
+//     marginBottom: 40,
+//     textAlign: 'center',
+//   },
+//   button: {
+//     backgroundColor: '#1e90ff',
+//     paddingVertical: 15,
+//     paddingHorizontal: 30,
+//     borderRadius: 12,
+//     shadowColor: '#000',
+//     shadowOpacity: 0.2,
+//     shadowOffset: { width: 0, height: 3 },
+//     shadowRadius: 4,
+//   },
+//   buttonText: {
+//     color: '#fff',
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//   },
+// });
 
 
 // import Calculator from '../src/utils/components/Calculator';
